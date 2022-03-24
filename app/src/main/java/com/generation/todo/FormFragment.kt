777291e17f382cc87494d6f7ff4dbb929f1f9a9a
Fernanda <fernandasmatiasi@gmail.com>
2022-03-24 16:54:21 -0300

    versão alterada

--- app/src/main/java/com/generation/todo/FormFragment.kt
+++ app/src/main/java/com/generation/todo/FormFragment.kt
@@ -1,6 +1,7 @@
 package com.generation.todo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,19 +10,29 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.generation.todo.databinding.FragmentFormBinding
 import com.generation.todo.databinding.FragmentListBinding
+import com.generation.todo.repository.Repository
 
 class FormFragment : Fragment() {
 
     private lateinit var binding: FragmentFormBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentFormBinding.inflate(
-            layoutInflater, container, false
-        )
+
+        binding = FragmentFormBinding.inflate(layoutInflater, container, false)
+
+        val repository = Repository()
+
+        mainViewModel = MainViewModel(repository)
+
+        mainViewModel.listCategoria()
+
+        mainViewModel.myCategoriaResponse.observe(viewLifecycleOwner){
+            response -> Log.d("Requisicao", response.body().toString())
+        }
 
         binding.buttonSalvar.setOnClickListener {
             findNavController().navigate(R.id.action_formFragment_to_listFragment)
--- app/src/main/java/com/generation/todo/ListFragment.kt
+++ app/src/main/java/com/generation/todo/ListFragment.kt
@@ -26,40 +26,7 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        binding = FragmentListBinding.inflate(
-            layoutInflater, container, false
-        )
-
-
-        //Gerando a listagem de tarefas com base na classe de modelo
-        val listTarefas = mutableListOf(
-            Tarefa(
-                "Limpar a Casa",
-                "Iniciar na parte da manhã",
-                "Henrique",
-                "2022-21-03",
-                true,
-                "Dia a Dia"
-            ),
-            Tarefa(
-                "Lavar a Louça",
-                "Do dia todo :^)",
-                "Henrique",
-                "2022-22-03",
-                false,
-                "Dia a Dia"
-            ),
-            Tarefa(
-                "Ir ao Parque",
-                "Me encontrar com o pessoal no metrô",
-                "Henrique",
-                "2022-26-03",
-                false,
-                "Lazer"
-            )
-        )
-
-        //Configurar o Adapter
+        binding = FragmentListBinding.inflate(layoutInflater, container, false)
 
         //Instaciar o adapter
         val adapter = TarefaAdapter()
@@ -72,9 +39,6 @@ class ListFragment : Fragment() {
 
         //Definir a lista para ter um tamanho fixo indepedente dos itens
         binding.recyclerTarefa.setHasFixedSize(true)
-
-        //Definindo quais vão ser os dados da lista
-        adapter.setLista(listTarefas)
 
         //Navegação para o Fragment de Form
         binding.floatingAdd.setOnClickListener {
